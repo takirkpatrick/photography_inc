6a1e94c3a69051c67f1227c9a3ff3ec86f4210ec
SC\alex.kirkpatrick <alex.kirkpatrick@my.simpson.edu>
2016-11-03 11:57:25 -0500

    renamed about us php file

--- resources/navigation.php
+++ resources/navigation.php
@@ -3,7 +3,7 @@ div style="overflow: auto; height: 45px;">
             <div class="container-fluid" style="width: 100%">
                     <ul class="nav navbar-nav" style="overflow: hidden; height: 45px">
                         <li> <a class="navbar-brand" href= "index.php"> Photography Inc </a> </li>
-                        <li> <a href= "about_us.html"> About Us </a> </li>
+                        <li> <a href= "about_us.php"> About Us </a> </li>
                         <li class="active" style="overflow: hidden; width: 150px;"> <a href= "camera_tutorials.php"> Camera Tutorials </a> </li>
                         <li> <a href= "hours.php"> Hours, Location, &amp; Directions </a> </li>
                         <li> <a href= "subject_ideas.php"> Subject and Location Ideas </a> </li>
